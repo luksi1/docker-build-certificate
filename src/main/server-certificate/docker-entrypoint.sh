87050681d94672d11c2801d35daaa8df5bab5cf8
Luke Simmons <luke.simmons@gmail.com>
2019-03-04 19:35:02 +0100

    export SERVER_NAME - problems with SERVER_NAME in other builds

--- src/main/server-certificate/docker-entrypoint.sh
+++ src/main/server-certificate/docker-entrypoint.sh
@@ -3,8 +3,10 @@
 
 if [ "${SERVER_SUBJECT}x" != "x" ]; then
   SERVER_NAME=$(echo "$SERVER_SUBJECT" | sed -r -n 's/.*CN=(.*)($|\/.*)/\1/p')
+  export SERVER_NAME
 elif [ "${COMMON_NAME}x" != "x" ]; then
   SERVER_NAME="$COMMON_NAME"
+  export SERVER_NAME
 else
   echo "You must indicate a SERVER_SUBJECT or a COMMON_NAME"
   exit 1
